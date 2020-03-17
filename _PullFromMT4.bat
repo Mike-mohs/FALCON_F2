1df8f5cf8100c7691a5d9d3cf83304678ecd5f3a
Mike-mohs <55208502+Mike-mohs@users.noreply.github.com>
2020-03-16 17:52:51 -0700

    Update _PullFromMT4.bat

--- _PullFromMT4.bat
+++ _PullFromMT4.bat
@@ -6,7 +6,7 @@ setlocal enabledelayedexpansion
 :: Source Directory where Expert Advisor is located
 set SOURCE_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\FALCON_F2"
 :: Destination Directory where Version Control Repository is located
-set DEST_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_F2"
+set DEST_DIR="C:\Users\Administrator\Documents\000_TradingRepo\FALCON_F2"
 
 :: Copy only files with *.mq4 extension
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
